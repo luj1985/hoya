3305411a5bd3006a37de87222041f66b19a4e1a4
Lu, Jun <luj1985@gmail.com>
2015-01-11 19:36:10 +0800

    initial commit

---
+++ app/scripts/lib/base.js
@@ -0,0 +1,308 @@
+/**
+ * Base lib for hoya.com
+ * By pansmaze@163.com
+ * 20140705
+ */
+(function(window, document, undefined) {
+    console = console || {
+        log: function() {
+            void 0;
+        }
+    };
+    window.HY = {
+        /**
+         * cookie
+         * cookie operation fn
+         * @ name       key for cookie (get|set)
+         * @ value      value for set a cookie
+         * @ options    detail options for set method
+         * ~ exp:
+         * get: w.cookie('name')
+         * set: w.cookie(’name’, ‘value’, {expires: 7, path: ‘/’, domain: ‘pay.sina.com.cn’, secure: true});
+         * delete: w.cookie('name', null)
+         */
+        cookie: function(name, value, options) {
+            if (typeof value != 'undefined') {
+                options = options || {};
+                if (value === null) {
+                    value = '';
+                    options.expires = -1;
+                }
+                var expires = '';
+                if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString())) { // expires can be passed as a Date obj
+                    var date;
+                    if (typeof options.expires == 'number') {
+                        date = new Date();
+                        date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+                        date = date.toUTCString();
+                    } else {
+                        date = options.expires;
+                    }
+                    expires = '; expires=' + date;
+                }
+                var path = options.path ? '; path=' + options.path : '';
+                var domain = options.domain ? '; domain=' + options.domain : '';
+                var secure = options.secure ? '; secure' : '';
+                document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+            } else {
+                var cookieValue = null;
+                if (document.cookie && document.cookie != '') {
+                    var cookies = document.cookie.split(';');
+                    for (var i = 0; i < cookies.length; i++) {
+                        var cookie = jQuery.trim(cookies[i]);
+                        // Does this cookie string begin with the name we want?
+                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                            break;
+                        }
+                    }
+                }
+                return cookieValue;
+            }
+        },
+        /**
+         * getUrlParam
+         * get url parameter by name
+         * @ name      key for the parameter
+         * @ str       if exists, use str instead of url
+         */
+        getUrlParam: function(name, str) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+            var search = ("string" == typeof str) ? str : window.location.search;
+            var r = search.substr(1).match(reg);
+            (r != null) ? r = decodeURIComponent(r[2]) : r = undefined;
+            return r;
+        },
+        getRdInt: function(maxVal) {
+            return Math.floor(Math.random() * (maxVal + 1));
+        },
+        htmlEncode: function(str) {
+            //return $('<div/>').text(str).html().replace(/&amp;/g, "&");
+            var dict = {
+                '&': "&amp;",
+                '<': "&lt;",
+                '>': "&gt;",
+                ' ': "&nbsp;",
+                '\'': "&#039;",
+                '"': "&quot;"
+            };
+            if ("string" !== typeof str) return str;
+            if (str.length == 0) return "";
+            return str.replace(/[&<> \'\"]/g, function(c) {
+                return dict[c];
+            });
+        },
+        /**
+         * htmlDecode
+         * decode html str
+         * @ str       decode it~
+         */
+        htmlDecode: function(val) {
+            return $('<div/>').html(val).text().replace(/\u00a0/g, "\u0020");
+        },
+        loading: function(loading) {
+            loading ? $('.loading,.loading_shade').removeClass('dn') : $('.loading,.loading_shade').addClass('dn');
+        },
+        loadimg: function(arr, funLoading, funOnLoad, funOnError) {
+
+            var numLoaded = 0,
+                numError = 0,
+                isObject = Object.prototype.toString.call(arr) === "[object Object]" ? true : false;
+
+            var arr = isObject ? arr.get() : arr;
+            for (a in arr) {
+                var src = isObject ? $(arr[a]).attr("data-src") : arr[a];
+                preload(src, arr[a]);
+            }
+
+            function preload(src, obj) {
+                var img = new Image();
+                img.onload = function() {
+                    numLoaded++;
+                    funLoading && funLoading(numLoaded, arr.length, src, obj);
+                    funOnLoad && numLoaded == arr.length && funOnLoad(numError);
+                };
+                img.onerror = function() {
+                    numLoaded++;
+                    numError++;
+                    funOnError && funOnError(numLoaded, arr.length, src, obj);
+                }
+                img.src = src;
+            }
+
+        },
+        // mountain
+        lib: {
+
+        }
+    };
+
+    /****** consummate jquery animate ******/
+    $.easing['jswing'] = $.easing['swing'];
+    $.extend($.easing, {
+        def: 'easeOutQuad',
+        swing: function(x, t, b, c, d) {
+            //alert($.easing.default);
+            return $.easing[$.easing.def](x, t, b, c, d);
+        },
+        easeInQuad: function(x, t, b, c, d) {
+            return c * (t /= d) * t + b;
+        },
+        easeOutQuad: function(x, t, b, c, d) {
+            return -c * (t /= d) * (t - 2) + b;
+        },
+        easeInOutQuad: function(x, t, b, c, d) {
+            if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+            return -c / 2 * ((--t) * (t - 2) - 1) + b;
+        },
+        easeInCubic: function(x, t, b, c, d) {
+            return c * (t /= d) * t * t + b;
+        },
+        easeOutCubic: function(x, t, b, c, d) {
+            return c * ((t = t / d - 1) * t * t + 1) + b;
+        },
+        easeInOutCubic: function(x, t, b, c, d) {
+            if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
+            return c / 2 * ((t -= 2) * t * t + 2) + b;
+        },
+        easeInQuart: function(x, t, b, c, d) {
+            return c * (t /= d) * t * t * t + b;
+        },
+        easeOutQuart: function(x, t, b, c, d) {
+            return -c * ((t = t / d - 1) * t * t * t - 1) + b;
+        },
+        easeInOutQuart: function(x, t, b, c, d) {
+            if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
+            return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
+        },
+        easeInQuint: function(x, t, b, c, d) {
+            return c * (t /= d) * t * t * t * t + b;
+        },
+        easeOutQuint: function(x, t, b, c, d) {
+            return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
+        },
+        easeInOutQuint: function(x, t, b, c, d) {
+            if ((t /= d / 2) < 1) return c / 2 * t * t * t * t * t + b;
+            return c / 2 * ((t -= 2) * t * t * t * t + 2) + b;
+        },
+        easeInSine: function(x, t, b, c, d) {
+            return -c * Math.cos(t / d * (Math.PI / 2)) + c + b;
+        },
+        easeOutSine: function(x, t, b, c, d) {
+            return c * Math.sin(t / d * (Math.PI / 2)) + b;
+        },
+        easeInOutSine: function(x, t, b, c, d) {
+            return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
+        },
+        easeInExpo: function(x, t, b, c, d) {
+            return (t == 0) ? b : c * Math.pow(2, 10 * (t / d - 1)) + b;
+        },
+        easeOutExpo: function(x, t, b, c, d) {
+            return (t == d) ? b + c : c * (-Math.pow(2, -10 * t / d) + 1) + b;
+        },
+        easeInOutExpo: function(x, t, b, c, d) {
+            if (t == 0) return b;
+            if (t == d) return b + c;
+            if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+            return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+        },
+        easeInCirc: function(x, t, b, c, d) {
+            return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b;
+        },
+        easeOutCirc: function(x, t, b, c, d) {
+            return c * Math.sqrt(1 - (t = t / d - 1) * t) + b;
+        },
+        easeInOutCirc: function(x, t, b, c, d) {
+            if ((t /= d / 2) < 1) return -c / 2 * (Math.sqrt(1 - t * t) - 1) + b;
+            return c / 2 * (Math.sqrt(1 - (t -= 2) * t) + 1) + b;
+        },
+        easeInElastic: function(x, t, b, c, d) {
+            var s = 1.70158;
+            var p = 0;
+            var a = c;
+            if (t == 0) return b;
+            if ((t /= d) == 1) return b + c;
+            if (!p) p = d * .3;
+            if (a < Math.abs(c)) {
+                a = c;
+                var s = p / 4;
+            } else var s = p / (2 * Math.PI) * Math.asin(c / a);
+            return -(a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
+        },
+        easeOutElastic: function(x, t, b, c, d) {
+            var s = 1.70158;
+            var p = 0;
+            var a = c;
+            if (t == 0) return b;
+            if ((t /= d) == 1) return b + c;
+            if (!p) p = d * .3;
+            if (a < Math.abs(c)) {
+                a = c;
+                var s = p / 4;
+            } else var s = p / (2 * Math.PI) * Math.asin(c / a);
+            return a * Math.pow(2, -10 * t) * Math.sin((t * d - s) * (2 * Math.PI) / p) + c + b;
+        },
+        easeInOutElastic: function(x, t, b, c, d) {
+            var s = 1.70158;
+            var p = 0;
+            var a = c;
+            if (t == 0) return b;
+            if ((t /= d / 2) == 2) return b + c;
+            if (!p) p = d * (.3 * 1.5);
+            if (a < Math.abs(c)) {
+                a = c;
+                var s = p / 4;
+            } else var s = p / (2 * Math.PI) * Math.asin(c / a);
+            if (t < 1) return -.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
+            return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p) * .5 + c + b;
+        },
+        easeInBack: function(x, t, b, c, d, s) {
+            if (s == undefined) s = 1.70158;
+            return c * (t /= d) * t * ((s + 1) * t - s) + b;
+        },
+        easeOutBack: function(x, t, b, c, d, s) {
+            if (s == undefined) s = 1.70158;
+            return c * ((t = t / d - 1) * t * ((s + 1) * t + s) + 1) + b;
+        },
+        easeInOutBack: function(x, t, b, c, d, s) {
+            if (s == undefined) s = 1.70158;
+            if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= (1.525)) + 1) * t - s)) + b;
+            return c / 2 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2) + b;
+        },
+        easeInBounce: function(x, t, b, c, d) {
+            return c - $.easing.easeOutBounce(x, d - t, 0, c, d) + b;
+        },
+        easeOutBounce: function(x, t, b, c, d) {
+            if ((t /= d) < (1 / 2.75)) {
+                return c * (7.5625 * t * t) + b;
+            } else if (t < (2 / 2.75)) {
+                return c * (7.5625 * (t -= (1.5 / 2.75)) * t + .75) + b;
+            } else if (t < (2.5 / 2.75)) {
+                return c * (7.5625 * (t -= (2.25 / 2.75)) * t + .9375) + b;
+            } else {
+                return c * (7.5625 * (t -= (2.625 / 2.75)) * t + .984375) + b;
+            }
+        },
+        easeInOutBounce: function(x, t, b, c, d) {
+            if (t < d / 2) return $.easing.easeInBounce(x, t * 2, 0, c, d) * .5 + b;
+            return $.easing.easeOutBounce(x, t * 2 - d, 0, c, d) * .5 + c * .5 + b;
+        }
+    });
+
+    $(function() {
+        // $('.language').on('click', 'a', function() {
+        //     var _la = $(this).attr('data-la'); // tw zh en
+
+        //     $('.language > a.current').removeClass('current');
+        //     $(this).addClass('current');
+
+        //     $('[data-' + _la + ']').each(function() {
+        //         $(this).removeClass('data-en data-tw data-zh');
+        //         $('body').removeClass('data-en data-tw data-zh');
+        //         !$(this).attr('data-tw') && $(this).attr('data-tw', HY.htmlEncode($(this).html()));
+        //         $(this).html($(this).attr('data-' + _la)).addClass('data-' + _la);
+        //         $('body').addClass('data-' + _la);
+        //     });
+        // });
+    });
+})(window, document);
---
+++ app/scripts/lib/tetris.js
@@ -0,0 +1,339 @@
+/*
+ * Teris by pansmaze@163.com
+ *
+ * shape:
+ *      1:          2:          3:          4:          5:          6:          7:  □       8: □
+ *          □□          □           □             □          □         □            □          □
+ *          □□         □□□          □□□         □□□         □□         □□           □          □
+ *                                                          □           □           □          □
+ *                                                                                             □
+ *
+ * orientation:
+ *      1: default 2: +90deg 3: +180deg 4: +270deg
+ *
+ *
+ */
+(function() {
+    HY.tetris = function(conf) {
+        var _this = this;
+        conf = $.extend({
+            container: $('#container'),
+            tclass: 'tetris',
+            tbclass: 'tetris-block',
+            tetris: [{
+                bgColor: '#ccc',
+                textColor: '#fff',
+                text: ['CGHP', 'LDTF', 'TZD2', 'QJNT'],
+                shape: 5,
+                orientation: 1,
+                aIndex: 2, // animate away index
+                aType: 1, // animate type ->: 1
+                x: 8,
+                y: 11
+            }],
+            tCaseData: {},
+            _dy: 500,
+            standard: 10, // 1个x/y坐标单位
+            speed: 100 // 单帧动画速度
+        }, conf);
+
+        function __getMyText(text) {
+            if (/[^0-9]/.test(text.slice(0, 1))) {
+                text = '<i>' + text.slice(0, 1) + '</i>' + text.slice(1);
+            } else if (text.length > 4) {
+                text = '<span style="line-height: 12px;word-break: break-all;position: absolute;width: 100%;left: 0;top: 12px;">' + text + '</span>';
+            }
+            return text;
+        }
+
+        function __generator(tdata) {
+            var __bottom = (conf._dy * 1.3 + tdata.y * conf.standard);
+            var __left = tdata.x * conf.standard;
+            var _html = '<div class="' + conf.tclass + '" data-aidx="' + tdata.aIndex + '" data-atype="' + tdata.aType + '" data-left="' + __left + '" data-bottom="' + Math.round(__bottom - conf._dy * 1.3) + '" style="bottom:' + Math.round(__bottom) + 'px;left:' + __left + 'px;">';
+            var _case = tdata.shape + '' + tdata.orientation;
+            var _matrix_pos = [];
+
+            switch (_case) {
+                case '11':
+                    _matrix_pos = [
+                        [0, 0], // left, top; x, y
+                        [1, 0],
+                        [0, 1],
+                        [1, 1]
+                    ];
+                    break;
+                case '21':
+                    _matrix_pos = [
+                        [1, 0],
+                        [0, 1],
+                        [1, 1],
+                        [2, 1]
+                    ];
+                    break;
+                case '22':
+                    _matrix_pos = [
+                        [0, 0],
+                        [0, 1],
+                        [1, 1],
+                        [0, 2]
+                    ];
+                    break;
+                case '23':
+                    _matrix_pos = [
+                        [0, 0],
+                        [1, 0],
+                        [2, 0],
+                        [1, 1]
+                    ];
+                    break;
+                case '24':
+                    _matrix_pos = [
+                        [1, 0],
+                        [0, 1],
+                        [1, 1],
+                        [1, 2]
+                    ];
+                    break;
+                case '31':
+                    _matrix_pos = [
+                        [0, 0],
+                        [0, 1],
+                        [1, 1],
+                        [2, 1]
+                    ];
+                    break;
+                case '32':
+                    _matrix_pos = [
+                        [0, 0],
+                        [1, 0],
+                        [0, 1],
+                        [0, 2]
+                    ];
+                    break;
+                case '33':
+                    _matrix_pos = [
+                        [0, 0],
+                        [1, 0],
+                        [2, 0],
+                        [2, 1]
+                    ];
+                    break;
+                case '34':
+                    _matrix_pos = [
+                        [1, 0],
+                        [1, 1],
+                        [0, 2],
+                        [1, 2]
+                    ];
+                    break;
+                case '41':
+                    _matrix_pos = [
+                        [2, 0],
+                        [0, 1],
+                        [1, 1],
+                        [2, 1]
+                    ];
+                    break;
+                case '42':
+                    _matrix_pos = [
+                        [0, 0],
+                        [0, 1],
+                        [0, 2],
+                        [1, 2]
+                    ];
+                    break;
+                case '43':
+                    _matrix_pos = [
+                        [0, 0],
+                        [1, 0],
+                        [2, 0],
+                        [0, 1]
+                    ];
+                    break;
+                case '44':
+                    _matrix_pos = [
+                        [0, 0],
+                        [1, 0],
+                        [1, 1],
+                        [1, 2]
+                    ];
+                    break;
+                case '51':
+                    _matrix_pos = [
+                        [1, 0],
+                        [0, 1],
+                        [1, 1],
+                        [0, 2]
+                    ];
+                    break;
+                case '52':
+                    _matrix_pos = [
+                        [0, 0],
+                        [1, 0],
+                        [1, 1],
+                        [2, 1]
+                    ];
+                    break;
+                case '61':
+                    _matrix_pos = [
+                        [0, 0],
+                        [0, 1],
+                        [1, 1],
+                        [1, 2]
+                    ];
+                    break;
+                case '62':
+                    _matrix_pos = [
+                        [1, 0],
+                        [2, 0],
+                        [0, 1],
+                        [1, 1]
+                    ];
+                    break;
+                case '71':
+                    _matrix_pos = [
+                        [0, 1],
+                        [0, 2],
+                        [0, 3],
+                        [0, 4]
+                    ];
+                    break;
+                case '72':
+                    _matrix_pos = [
+                        [1, 0],
+                        [2, 0],
+                        [3, 0],
+                        [4, 0]
+                    ];
+                    break;
+                case '81':
+                    _matrix_pos = [
+                        [0, 1],
+                        [0, 2],
+                        [0, 3],
+                        [0, 4],
+                        [0, 5]
+                    ];
+                    break;
+                case '82':
+                    _matrix_pos = [
+                        [1, 0],
+                        [2, 0],
+                        [3, 0],
+                        [4, 0],
+                        [5, 0]
+                    ];
+                    break;
+                default:
+                    _matrix_pos = [];
+            }
+
+            for (var i = 0; i < _matrix_pos.length; i++) {
+                var cssDict = {
+                    // ta: ['left', 'center', 'right'],
+                    ta: ['center', 'center', 'center'],
+                    // lh: ['25px', '43px', '62px']
+                    lh: ['60px', '60px', '60px']
+                }
+                var randomCss = 'text-align:' + cssDict.ta[HY.getRdInt(2)] + ';line-height:' + cssDict.lh[HY.getRdInt(2)] + ';';
+
+                var thumbnail_img = tdata.text[i] === '' ? '' : ('<span class="case_title" style="width: ' + conf.tCaseData[tdata.text[i]].text.replace(/[^a-z0-9A-Z\s]/g, 'AA').length * 7.23 + 'px;">' + conf.tCaseData[tdata.text[i]].text + '</span><img class="case_thumbnail" data-src="http://localhost:9012/images/case/' + tdata.text[i] + '/a.jpg?20140629" alt="' + conf.tCaseData[tdata.text[i]].text + '" />');
+
+                _html += '<span class="' + conf.tbclass + '" style="color:' + tdata.textColor + ';background-color:' + tdata.bgColor + ';top:' + _matrix_pos[i][1] * conf.standard + 'px;left:' + _matrix_pos[i][0] * conf.standard + 'px;' + randomCss + '" data-case="' + tdata.text[i] + '"' + ((tdata.text[i] !== '' && conf.tCaseData[tdata.text[i]]) ? (' data-year="' + conf.tCaseData[tdata.text[i]].year + '" data-category="' + conf.tCaseData[tdata.text[i]].category) : '') + '">' + thumbnail_img + __getMyText(tdata.text[i]) + '</span>';
+            }
+
+            _html += '</div>';
+            conf.container.append(_html);
+        }
+
+        function __animate() {
+            var _tetris = conf.container.children('.' + conf.tclass);
+            var _mry = Math.round(conf._dy * 1.3);
+
+            _tetris.each(function(key) {
+                var _aem = $(this);
+                setTimeout(function() {
+                    _aem.animate({
+                        'bottom': ['-=' + _mry + 'px', 'easeOutExpo']
+                    }, conf.speed);
+                }, key * conf.speed * 0.93);
+            });
+        }
+
+        _this.flyAway = function(fatime, delay) {
+            var _tetris = conf.container.children('.' + conf.tclass);
+            _tetris.stop(true, true);
+
+            var _sprt = 0;
+            var _ww = $(window).width();
+            var _wh = $(window).height();
+            var _adict = ['0', {
+                'left': ['+=' + _ww + 'px', 'easeInQuad']
+            }, {
+                'bottom': ['-=' + _wh + 'px', 'easeInQuad']
+            }, {
+                'left': ['-=' + _ww + 'px', 'easeInQuad']
+            }, {
+                'bottom': ['+=' + _ww + 'px', 'easeInQuad']
+            }];
+            for (var i = 1; i <= conf.tetris.length; i++) {
+                _sprt += delay;
+                var this_t = conf.container.children('.' + conf.tclass + '[data-aidx=' + i + ']');
+                // console.log(_sprt)
+                this_t.delay(_sprt).animate(_adict[parseInt(this_t.attr('data-atype'), 10)], fatime);
+            }
+        }
+
+        _this.reset = function() {
+            conf.container.children('.' + conf.tclass).each(function() {
+                $(this).css('left', $(this).attr('data-left') + 'px');
+                $(this).css('bottom', $(this).attr('data-bottom') + 'px');
+            });
+        }
+
+        _this.init = function(callback) {
+            $.each(conf.tetris, function(key, val) {
+                __generator(val);
+            });
+
+            $('.case_thumbnail').hover(function() {
+                var $this = $(this);
+
+                var $case_title = $this.siblings('.case_title');
+                var _oft = $this.offset();
+                var _case = $this.attr('data-case');
+                _oft.dtop = _oft.top + 60;
+                _oft.top = _oft.top + 10;
+                _oft.dleft = _oft.left + 55;
+                _oft.left = _oft.left + 10;
+                $('body').on('mousemove.c_t' + _case, function(e) {
+                    if (_oft.top < e.pageY && _oft.dtop > e.pageY && _oft.left < e.pageX && _oft.dleft > e.pageX) {
+                        $case_title.offset({
+                            top: e.pageY - 10,
+                            left: e.pageX
+                        });
+                    } else {
+                        $('body').off('mousemove.c_t' + _case);
+                        $case_title.css('z-index', '-1');
+                        setTimeout(function() {
+                            $case_title.css('z-index', '101');
+                        }, 88);
+                    }
+                });
+            }, function(e) {
+                // if (!$(e.relatedTarget).hasClass('case_title')) {
+                //     $('body').off('mousemove.c_t');
+                // }
+            });
+            callback();
+            return _this;
+        };
+
+        _this.start = function(callback) {
+            __animate();
+            setTimeout(function() {
+                callback();
+            }, conf.speed * conf.tetris.length);
+        };
+    };
+})();
