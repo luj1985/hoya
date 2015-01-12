07d93c63d6c249d7d81c06c6629245e93a85b070
Lu, Jun <luj1985@gmail.com>
2015-01-13 02:01:53 +0800

    to use one set of colors

--- app/scripts/app/navigation.js
+++ app/scripts/app/navigation.js
@@ -1,29 +1,4 @@
 $(function() {
-  $('.nav-item').click(function() {
-    var this_list = $(this).attr('data-list');
-
-    var _order = $(this).attr('data-order');
-    $('.nav-item.active').removeClass('active');
-    $('.nav-item[data-order=' + _order + ']').addClass('active');
-
-    $('.nav-list > li.active').click();
-
-    if (this_list) {
-      if (!$(this_list).hasClass('active')) {
-        $('.nav-list.active').animate({
-          'bottom': ['-600px', 'easeInExpo']
-        }, 200).removeClass('active');
-        $(this_list).animate({
-          'bottom': ['0px', 'easeOutExpo']
-        }, 200).addClass('active');
-      }
-    } else if ($(this).attr('data-fake') === 'yes') {
-      var _idx = $(this).parent().children('.nav-item').index($(this));
-      $('#main').find('.nav-item').eq(_idx).attr('data-delay_click', 'yes');
-    }
-  });
-
-
   $('.nav-list > li').hover(function() {
     var _key = $(this).attr('data-year') ? {
       key: 'data-year',
@@ -35,14 +10,11 @@ $(function() {
 
     $('.nav-list > li.active').removeClass('active');
     $(this).addClass('active');
-    $('#my_index_tetris').find('.tetris-block').addClass('tetris-transparent').end().find('.tetris-block[' + _key.key + '=' + _key.val + ']').removeClass('tetris-transparent');
-  }, function() {
-    $('body').one('click', function(e) {
-      if (!$(e.target).parents('.nav-list').length) {
-
-        $('.nav-list > li.active').removeClass('active');
-        $('#my_index_tetris').find('.tetris-block').removeClass('tetris-transparent');
-      }
-    });
+    $('#my_index_tetris')
+      .find('.tetris-block')
+      .addClass('tetris-transparent')
+    .end()
+      .find('.tetris-block[' + _key.key + '=' + _key.val + ']')
+      .removeClass('tetris-transparent');
   });
 });
