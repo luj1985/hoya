50680568970e4ba5405542a28e8b26b18c4a56c4
Lu, Jun <luj1985@gmail.com>
2015-01-17 15:41:18 +0800

    refine team page

--- app/scripts/app/page.team.js
+++ app/scripts/app/page.team.js
@@ -1,12 +1,20 @@
 $(function() {
-  $('.team-name-list').on('click', 'li', function() {
-    var page = $(this);
-    var _lis = page.parent().children('li');
-    var _idx = _lis.index(page);
-    // console.log(_idx)
-    _lis.removeClass('active');
-    page.addClass('active');
+  // $('.team-name-list').on('click', 'li', function() {
+  //   var page = $(this);
+  //   var _lis = page.parent().children('li');
+  //   var _idx = _lis.index(page);
+  //   // console.log(_idx)
+  //   _lis.removeClass('active');
+  //   page.addClass('active');
 
-    page.parent().siblings('.team-detail').addClass('dn').eq(_idx).removeClass('dn');
+  //   page.parent().siblings('.team-detail').addClass('dn').eq(_idx).removeClass('dn');
+  // });
+  $('#team .names').on('click', 'a', function(e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $('#team .names li').removeClass('active');
+    $('#team article').removeClass('active');
+    $(this).closest('li').addClass('active');
+    $(target).addClass('active');
   });
 });
