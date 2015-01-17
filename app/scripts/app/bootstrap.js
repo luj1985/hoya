a7ca2fd7cb39845faf1c4523316fe33452cfa15c
Lu, Jun <luj1985@gmail.com>
2015-01-17 12:42:47 +0800

    remove jquery.hash dependency

--- app/scripts/app/bootstrap.js
+++ app/scripts/app/bootstrap.js
@@ -2,19 +2,29 @@ $(function() {
   var PAGE_INITED = $.PAGE_INITED;
   var PAGES = $.PAGES;
 
+  
+  $.fn.classed = function(cls, predictor) {
+    this.each(function() {
+      var page = $(this);
+      if (predictor(page)) {
+        page.addClass(cls);
+      } else {
+        page.removeClass(cls);
+      }
+    });
+  }
+
   $('.page').each(function() {
     var name = $(this).attr('id');
     PAGE_INITED[name] = false;
   });
 
-  $(window).hashChange(function() {
+  $(window).on('hashchange', function(e) {
     var hash = window.location.hash.replace('#', '') || 'main';
     var names = hash.split('_');
     var name = names[0], param = names[1];
     var loader = PAGES['_load_' + name];
     loader && loader(param);
   });
-  // window.location.hash = '';
-  $(window).hashChange();
-  // $.loading(true);
+  $(window).trigger('hashchange');
 });
