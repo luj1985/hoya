16ed146177a9e37f0b6711fbc953bfeb5d74ec07
Lu, Jun <luj1985@gmail.com>
2015-04-14 00:19:42 +0800

    remove not used code

--- app/scripts/app/jquery.loader.js
+++
@@ -1,8 +0,0 @@
-(function($, undefined) {
-  'use strict';
-
-  $.loading = function(loading) {
-    var loader = $('.loading');
-    loading ? loader.removeClass('hide') : loader.addClass('hide');
-  };
-})(jQuery);
